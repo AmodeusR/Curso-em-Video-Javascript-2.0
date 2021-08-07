b42c1bf07510378e528e69d541909fc4e1dcc451
Ricardo Magalhães <ricardogleiss@hotmail.com>
2021-08-07 18:01:29 -0300

    Correção de bugs
    Correção de falha no código em que os dados não eram validados corretamente.

--- Aulas/Aula 14ex/ex016/script.js
+++ Aulas/Aula 14ex/ex016/script.js
@@ -1,42 +1,45 @@
 document.querySelector(".botão").addEventListener("click", contar);
 let resultado = document.querySelector(".resultado");
+
 function contar() {
   let início = document.querySelector("#inicio").value;
   let fim =   document.querySelector("#fim").value;
   let passo = document.querySelector("#passo").value;
 
-  if (início.length == 0 || fim.length == 0) {
-    resultado.innerHTML = "Impossível contar!";
+  if (início.length == 0 || fim.length == 0 || passo.length == 0) {
+    resultado.innerHTML = "Impossível contar! Cheque os campos.";
+    return 0;
   } else if (passo == 0) {
     alert("Passo inválido! Considerando passo 1");
     passo = 1;
-  }
+  } 
   
   let inícioN = Number(início);
   let fimN = Number(fim);
   let passoN = Number(passo);
 
-  if (inícioN < fimN) {
-    resultado.innerHTML = `${inícioN} 👉 `;
-    while(inícioN + passoN <= fimN) {
-      inícioN += passoN;
+    if (inícioN < fimN) {
+      resultado.innerHTML = `${inícioN} 👉 `;
+      while(inícioN + passoN <= fimN) {
+        inícioN += passoN;
 
-      if(inícioN + passoN <= fimN) {
-      resultado.innerHTML += `${inícioN} 👉 `;
-      } else {
-        resultado.innerHTML += `${inícioN} 🏁`;
+        if(inícioN + passoN <= fimN) {
+          resultado.innerHTML += `${inícioN} 👉 `;
+        } else {
+          resultado.innerHTML += `${inícioN} 🏁`;
+        }
       }
-    }
-  } else {
-    resultado.innerHTML = `${inícioN} 👉 `;
-    while(inícioN - passoN >= fimN) {
-      inícioN -= passoN;
-      
-      if (inícioN - passoN >= fimN) {
-        resultado.innerHTML += `${inícioN} 👉 `;
-      } else {
-        resultado.innerHTML += `${inícioN} 🏁`;
+    } else {
+      resultado.innerHTML = `${inícioN} 👉 `;
+      while(inícioN - passoN >= fimN) {
+        inícioN -= passoN;
+        
+        if (inícioN - passoN >= fimN) {
+          resultado.innerHTML += `${inícioN} 👉 `;
+        } else {
+          resultado.innerHTML += `${inícioN} 🏁`;
+        }
       }
-    }
+    
   }
 }
